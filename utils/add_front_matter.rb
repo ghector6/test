b12de7da8e83fcf161036404a917e97daae884cf
Hector Garcia Rosales <ghectormus@gmail.com>
2024-10-01 14:09:53 -0600

    add: function that will create ftoint matter structure

--- utils/add_front_matter.rb
+++ utils/add_front_matter.rb
@@ -4,6 +4,14 @@ require 'date'
 dir = 'content/'
 files = Dir.glob("#{dir}**/*.md")
 
-files.each do |file|
-  puts file
-end 
+# files.each do |file|
+#   puts file
+# end 
+
+#add front matter to each file
+def generate_front_matter(file_name)
+  title = file_name.gsub(/[-_]/, ' ').gsub('.md', '').capitalize
+end
+
+result = generate_front_matter("Director_de_Tecnologia_e_inovacion.md")
+puts result
